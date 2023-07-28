f0dbbad688f2a4dc252fd8648246c26a5ee3c348
Sunjae Bae <sunjae.bae1@gmail.com>
2023-07-28 09:11:04 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -21,29 +21,29 @@ preproc <- function(df, n.levels=10, na.values=c(-1,-9), no_factor=NULL) {
 		      
 
 ## Plot the object derived from tune.rsfrc
-library(akima)
-plot.tune <- function(o, linear = TRUE) {
-	x <- o$results[,1]
-	y <- o$results[,2]
-	z <- o$results[,3]
-	so <- interp(x=x, y=y, z=z, linear = linear)
-	idx <- which.min(z)
-	x0 <- x[idx]
-	y0 <- y[idx]
-	filled.contour(x = so$x,
-	y = so$y,
-	z = so$z,
-	xlim = range(so$x, finite = TRUE) + c(-2, 2),
-	ylim = range(so$y, finite = TRUE) + c(-2, 2),
-	color.palette =
-	colorRampPalette(c("yellow", "red")),
-	xlab = "nodesize",
-	ylab = "mtry",
-	main = "OOB error for nodesize and mtry",
-	key.title = title(main = "OOB error", cex.main = 1),
-	plot.axes = {axis(1);axis(2);points(x0,y0,pch="x",cex=1,font=2);
-	points(x,y,pch=16,cex=.25)})
-}
+# library(akima)
+# plot.tune <- function(o, linear = TRUE) {
+# 	x <- o$results[,1]
+# 	y <- o$results[,2]
+# 	z <- o$results[,3]
+# 	so <- interp(x=x, y=y, z=z, linear = linear)
+# 	idx <- which.min(z)
+# 	x0 <- x[idx]
+# 	y0 <- y[idx]
+# 	filled.contour(x = so$x,
+# 	y = so$y,
+# 	z = so$z,
+# 	xlim = range(so$x, finite = TRUE) + c(-2, 2),
+# 	ylim = range(so$y, finite = TRUE) + c(-2, 2),
+# 	color.palette =
+# 	colorRampPalette(c("yellow", "red")),
+# 	xlab = "nodesize",
+# 	ylab = "mtry",
+# 	main = "OOB error for nodesize and mtry",
+# 	key.title = title(main = "OOB error", cex.main = 1),
+# 	plot.axes = {axis(1);axis(2);points(x0,y0,pch="x",cex=1,font=2);
+# 	points(x,y,pch=16,cex=.25)})
+# }
 
 
 ## Wrapper for xgb.cv
