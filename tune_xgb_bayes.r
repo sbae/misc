e8df9e236a4802040c13be0360f51c723c5663b1
Sunjae Bae <sunjae.bae1@gmail.com>
2025-07-08 23:40:57 -0400

    Update tune_xgb_bayes.r

--- tune_xgb_bayes.r
+++ tune_xgb_bayes.r
@@ -120,13 +120,15 @@ tune_xgb_bayes <- function(data, label, param_bounds = NULL, n_fold = 5,
     }
 
     # Overwrite defaults with user-provided bounds for known parameters
-    for (param in names(intersect(names(param_bounds), names(default_bounds)))) {
+    for (param in intersect(names(param_bounds), names(default_bounds))) {
       final_bounds[[param]] <- param_bounds[[param]]
-      cat("param_bounds [", param, "] updated with ", param_bounds[[param]])
+      cat(sprintf("Using user-defined bounds for '%s': [%s, %s]\n",
+                  param, final_bounds[[param]][1], final_bounds[[param]][2]))
     }
   }
-
-  print(final_bounds)
+  cat("--- Parameter bounds ---\n")
+  cat(final_bounds, "\n")
+  
   # --- 5. Run Bayesian Optimization ---
   cat("--- Starting Bayesian Optimization ---\n")
   optimizer <- bayesOpt(
